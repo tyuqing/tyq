4875afa31075ce8b3a2f9bb5dff70c5bcc17675c
tianyuqing <543997782@qq.com>
2015-08-13 17:33:43 +0800

    绿色背景 菜单栏

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+$(function(){
+  /*菜单栏*/
+  $("#nav").click(function(e){
+    var target = $(event.target);
+    if(target.hasClass("act")) {
+      return false;
+    }else {
+      $(".act").removeClass("act");
+      target.addClass("act");
+    }
+  });
+});
