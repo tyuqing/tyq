87458b1230b1eb0026904e270681e2e42c6a4c2e
tianyuqing <543997782@qq.com>
2015-08-14 16:23:02 +0800

    红色背景 三个页面切换

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,22 @@ $(function(){
   /*菜单栏*/
   $("#nav").click(function(e){
     var target = $(event.target);
+    var targetId = target.attr("id");
     if(target.hasClass("act")) {
       return false;
     }else {
       $(".act").removeClass("act");
       target.addClass("act");
+      if(targetId == "labout") {
+        $("#bg-div").animate({"left": "0%"}, 1000);
+        $("#content").animate({"left": "0%"}, 1000);
+      }else if(targetId == "lresume") {
+        $("#bg-div").animate({"left": "-88%"}, 1000);
+        $("#content").animate({"left": "-88%"}, 1000);
+      }else if(targetId == "lproject") {
+        $("#bg-div").animate({"left": "-179%"}, 1000);
+        $("#content").animate({"left": "-179%"}, 1000);
+      }
     }
   });
 });
