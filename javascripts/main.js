9bd6037c38484eb001c7893c9f83329434f06127
tianyuqing <543997782@qq.com>
2015-12-27 17:39:47 +0800

    页面结构调整了一下

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 $(function(){
   /*菜单栏*/
-  $("#nav").click(function(e){
+  $("nav").click(function(e){
     var target = $(event.target);
     var targetId = target.attr("id");
     if(target.hasClass("act")) {
@@ -8,14 +8,14 @@ $(function(){
     }else {
       $(".act").removeClass("act");
       target.addClass("act");
-      if(targetId == "labout") {
-        $("#bg-div").animate({"left": "0%"}, 1000);
+      if(targetId == "nav-about") {
+        $("#bg-card").animate({"left": "0%"}, 1000);
         $("#content").animate({"left": "0%"}, 1000);
-      }else if(targetId == "lresume") {
-        $("#bg-div").animate({"left": "-88%"}, 1000);
+      }else if(targetId == "nav-resume") {
+        $("#bg-card").animate({"left": "-88%"}, 1000);
         $("#content").animate({"left": "-88%"}, 1000);
-      }else if(targetId == "lproject") {
-        $("#bg-div").animate({"left": "-179%"}, 1000);
+      }else if(targetId == "nav-project") {
+        $("#bg-card").animate({"left": "-179%"}, 1000);
         $("#content").animate({"left": "-179%"}, 1000);
       }
     }
