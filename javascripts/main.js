769f4fa4d27350094f3ba8636152edcc689ae1da
tianyuqing <543997782@qq.com>
2015-12-28 22:29:03 +0800

    作品页面 项目文字部分
    未完成

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,7 +6,7 @@ $(function(){
     if(target.hasClass("act")) {
       return false;
     }else {
-      $(".act").removeClass("act");
+      target.siblings(".act").removeClass("act");
       target.addClass("act");
       if(targetId == "nav_about") {
         $("#bg_card").animate({"left": "0%"}, 1000);
@@ -18,6 +18,32 @@ $(function(){
         $("#bg_card").animate({"left": "-179%"}, 1000);
         $("#content").animate({"left": "-179%"}, 1000);
       }
+    }
+  });
+  /* 作品的菜单栏 */
+  $('#proj_menu').click(function(e) {
+    var target = $(event.target);
+    var targetId = target.attr("id");
+    if(target.hasClass("active")) {
+      return false;
+    }else {
+      target.siblings(".active").removeClass("active");
+      target.addClass("active");
+      $(".proj-body").addClass("hidden");
+      /*所点按钮对应的项目内容*/
+      var targetProjBody = $("#" + targetId.replace("menu_", ""));
+      targetProjBody.removeClass("hidden");
+    }
+  });
+  /* 作品图片的选择菜单栏 */
+  $('.proj_img_control').click(function(e) {
+    var target = $(event.target);
+    var targetId = target.attr("id");
+    if(target.hasClass("current")) {
+      return false;
+    }else {
+      target.siblings(".current").removeClass("current");
+      target.addClass("current");
     }
   });
 });
