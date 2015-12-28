f6884143adc01394a2917d5988a0d9e663295e56
tianyuqing <543997782@qq.com>
2015-12-28 19:11:04 +0800

    写技能和作品页面的内容
    未完成

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,14 +8,14 @@ $(function(){
     }else {
       $(".act").removeClass("act");
       target.addClass("act");
-      if(targetId == "nav-about") {
-        $("#bg-card").animate({"left": "0%"}, 1000);
+      if(targetId == "nav_about") {
+        $("#bg_card").animate({"left": "0%"}, 1000);
         $("#content").animate({"left": "0%"}, 1000);
-      }else if(targetId == "nav-resume") {
-        $("#bg-card").animate({"left": "-88%"}, 1000);
+      }else if(targetId == "nav_skill") {
+        $("#bg_card").animate({"left": "-88%"}, 1000);
         $("#content").animate({"left": "-88%"}, 1000);
-      }else if(targetId == "nav-project") {
-        $("#bg-card").animate({"left": "-179%"}, 1000);
+      }else if(targetId == "nav_project") {
+        $("#bg_card").animate({"left": "-179%"}, 1000);
         $("#content").animate({"left": "-179%"}, 1000);
       }
     }
